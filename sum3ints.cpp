07c25cc9646310b0dc8b24970eaa9114b6eef35c
John Caruthers <john.d.caruthers@gmail.com>
2021-12-18 03:00:27 -0500

    created, intial commit

---
+++ sum3ints.cpp
@@ -0,0 +1,10 @@
+int main() {
+    // get integers
+    int int1, int2, int3, outint;
+    std::cin >> int1 >> int2 >> int3;
+    
+    // Return the sum of the integers
+    std::cout << int1 + int2 + int3;
+    
+    return 0;
+}
