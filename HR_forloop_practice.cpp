b53e3245acd24ae701144b45068bb48389955174
John Caruthers <john.d.caruthers@gmail.com>
2021-12-19 05:07:10 -0500

    complete

--- HR_forloop_practice.cpp
+++ HR_forloop_practice.cpp
@@ -11,11 +11,20 @@ int main() {
     for(int dummy_i = a; dummy_i <= b; dummy_i++) {
         // for numbers less than 10, print word form
         if(dummy_i < 10) {
-            std::cout << dummy_i << " is less than 10!\n";
+            if(dummy_i == 9) {std::cout << "nine\n";}
+            else if(dummy_i == 8) {std::cout << "eight\n";}
+            else if(dummy_i == 7) {std::cout << "seven\n";}
+            else if(dummy_i == 6) {std::cout << "six\n";}
+            else if(dummy_i == 5) {std::cout << "five\n";}
+            else if(dummy_i == 4) {std::cout << "four\n";}
+            else if(dummy_i == 3) {std::cout << "three\n";}
+            else if(dummy_i == 2) {std::cout << "two\n";}
+            else {std::cout << "one\n";}        
         }
         // for numbers 10 and greater, state even or odd
         else {
-            std::cout << dummy_i << " is greater than 9!\n";
+            if(dummy_i % 2 == 1) {std::cout << "odd\n";}
+            else {std::cout << "even\n";}
         }
     }
 
