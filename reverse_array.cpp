9219fe4baba7c488f2813665d3b538a53ac2e93d
John Caruthers <john.d.caruthers@gmail.com>
2021-12-25 23:59:18 -0500

    initial commit

---
+++ reverse_array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    // get size of array
+    int n;
+    std::cin >> n;
+    
+    //build array
+    int arr[n];
+    for(int dummy_i = 0; dummy_i < n; dummy_i++) {
+        std::cin >> arr[dummy_i];
+    }
+    
+    
+    //outpur array in reverse order
+    for(int dummy_o = n - 1; dummy_o >=0; dummy_o--) {
+        std::cout << arr[dummy_o] << " ";
+    }
+
+    return 0;
+}
