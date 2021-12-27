06196285fa92cf0e13f7e0a766671e72efba9947
John Caruthers <john.d.caruthers@gmail.com>
2021-12-27 14:42:03 -0500

    passed all but two tests
    doens't work when there are only two different scores and the length is three

--- nestedLists.py
+++ nestedLists.py
@@ -10,5 +10,27 @@ if __name__ == '__main__':
     records.sort(key = lambda ind_score: ind_score[1])
     
     # get all second lowest scores with names
+    temp_scores = []
+    temp_names = []
+    for record in records:
+        temp_names.append(record[0])
+        temp_scores.append(record[1])
     
-    # sort names and print
+    print(temp_scores)
+    
+    # Go through scores and get the second lowest grade, set index
+    shs = 0
+    shsIndex = 0
+    for i in range(len(temp_scores)):
+        pass
+
+    # get indexes with the highest score
+    shNames = []
+    for i in range(0, len(temp_scores)):
+        if temp_scores[i] == shs:
+            shNames.append(temp_names[i])
+    
+    # sort and print
+    shNames.sort()
+    for i in shNames:
+        print(i)
