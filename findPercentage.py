d902919c530fffd5c9e5d43c5ff6523e1f6a381a
John Caruthers <john.d.caruthers@gmail.com>
2021-12-28 09:22:46 -0500

    initial commit
    learning about * and ** prefix operators

---
+++ findPercentage.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    # num_students = int(input())
+    # student_marks = {}
+    # for _ in range(num_students):
+    #     name, *line = input().split()
+    #     scores = list(map(float, line))
+    #     student_marks[name] = scores
+    # query_name = input()
+    # print(student_marks)
+    num_students = int(input())
+    student_marks = {}
+    name, *line = input().split()
+    print("Name: " + name)
+    print(*line)
+    
