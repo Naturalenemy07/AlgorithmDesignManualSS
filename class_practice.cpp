1bfb29f0870349c8343a9afeefb380ed31933791
John Caruthers <john.d.caruthers@gmail.com>
2022-01-01 23:49:51 -0500

    intial commit

---
+++ class_practice.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <sstream>
+
+class Student {
+    private:
+        int age;
+        std::string first_name;
+        std::string last_name;
+        int standard;
+    public:
+        void set_age(int public_age) {
+            age = public_age;
+        }
+        void set_standard(int public_standard) {
+            standard = public_standard;
+        }
+        void set_first_name(std::string public_first_name) {
+            first_name = public_first_name;
+        }
+        void set_last_name(std::string public_last_name) {
+            last_name = public_last_name;
+        }
+        
+        int get_age() {
+            return age;
+        }
+        int get_standard() {
+            return standard;
+        }
+        std::string get_first_name() {
+            return first_name;
+        }
+        std::string get_last_name() {
+            return last_name;
+        }
+        std::string to_string() {
+            std::string return_statement;
+            //return_statement = age + "," + first_name + "," + last_name + "," + standard;
+            return return_statement
+        }
+};
+
+int main() {
+    int age, standard;
+    std::string first_name, last_name;
+    
+    std::cin >> age >> first_name >> last_name >> standard;
+    
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+    
+    std::cout << st.get_age() << "\n";
+    std::cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    std::cout << st.get_standard() << "\n";
+    std::cout << "\n";
+    std::cout << st.to_string();
+    
+    return 0;
+}
