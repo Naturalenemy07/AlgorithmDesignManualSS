6884cecafa45c3a46b38cd238aa4b468bd71f159
John Caruthers <john.d.caruthers@gmail.com>
2022-01-10 05:11:25 -0500

    added print_full_name

--- string_prac.py
+++ string_prac.py
@@ -4,3 +4,8 @@ def swap_case(s):
 def split_and_join(line):
     ss_line = "-".join(line.split(" "))
     return ss_line
+
+def print_full_name(first, last):
+    print("Hello", first, last + "!", "You just delved into python.")
+    return
+    
