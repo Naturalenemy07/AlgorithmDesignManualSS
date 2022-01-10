07fd0ca5611b74f12e53d4dfcd6b244f5e091cb9
John Caruthers <john.d.caruthers@gmail.com>
2022-01-10 05:32:18 -0500

    added count_substring
    tried to not use find, seemed not too useful in this instance

--- string_prac.py
+++ string_prac.py
@@ -9,3 +9,14 @@ def print_full_name(first, last):
     print("Hello", first, last + "!", "You just delved into python.")
     return
     
+def count_substring(string, sub_string):
+    # get lengths to prevent index errors, reduce repetitive calculations
+    lensub = len(sub_string)
+    length = len(string) - lensub
+    
+    # go through string and find substring match
+    matches = 0
+    for elem in range(length+1):
+        if string[elem:elem+lensub] == sub_string:
+            matches += 1
+    return matches
